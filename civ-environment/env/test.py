1171adc9c121072dfdc29eb1133a462f8ae3f743
Anne Mykland <amykland@college.harvard.edu>
2024-12-12 01:41:12 -0500

    updating hyperparameter testing

--- civ-environment/env/test.py
+++ civ-environment/env/test.py
@@ -99,9 +99,9 @@ def main():
             "method": "random",
             "metric": {"goal": "maximize", "name": "score"},
             "parameters": {
-                "batch_size": {"values": [5, 10, 15, 20, 25]},
-                "K": {"values": [5, 10, 15, 20]},
-                "lr": {"values": [0.001, 0.005, 0.01, 0.05, 0.1]}
+                "batch_size": {"values": [5, 10, 15, 20, 25, 30]},
+                "K": {"values": [5, 10, 15, 20, 25, 30]},
+                "lr": {"values": [0.00005, 0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1]}
             },
             "project": "hyperparameter-sweep",  # Project name in WandB
             "name": "my_sweep" 
