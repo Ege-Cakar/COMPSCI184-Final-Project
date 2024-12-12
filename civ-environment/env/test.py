8730e4994b519200d6df5bc26306d66dc6a83d4b
Anne Mykland <amykland@college.harvard.edu>
2024-12-12 02:20:56 -0500

    hyperparams

--- civ-environment/env/test.py
+++ civ-environment/env/test.py
@@ -96,12 +96,15 @@ def main():
 
 
         sweep_configuration = {
-            "method": "random",
+            "method": "bayes",
             "metric": {"goal": "maximize", "name": "score"},
             "parameters": {
                 "batch_size": {"values": [5, 10, 15, 20, 25, 30]},
                 "K": {"values": [5, 10, 15, 20, 25, 30]},
-                "lr": {"values": [0.00005, 0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1]}
+                "lr": {"distribution": "log_uniform", "min": 0.0001, "max": 0.1}
+                #"batch_size": {"min": 5, "max": 30},
+                #"K": {"min": 5, "max": 30},
+                #"lr": {"min": 0.00005, "max": 0.05}
             },
             "project": "hyperparameter-sweep",  # Project name in WandB
             "name": "my_sweep" 
